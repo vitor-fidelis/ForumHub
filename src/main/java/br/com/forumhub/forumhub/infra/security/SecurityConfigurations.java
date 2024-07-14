dd20e6043e75f098f534e9b2d1f965926f693512
vifid <vitor.gabrielcoelhofidelis@gmail.com>
2024-07-14 21:30:08 +0100

    fixing

--- src/main/java/br/com/forumhub/forumhub/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/forumhub/forumhub/infra/security/SecurityConfigurations.java
@@ -29,6 +29,7 @@ public class SecurityConfigurations {
                         .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                         .authorizeHttpRequests(req -> {
                             req.requestMatchers("/login").permitAll();
+                            req.requestMatchers("/v3/api-doc/**", "/swagger-ui/**").permitAll();
                             req.anyRequest().authenticated();
                         })
                         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
